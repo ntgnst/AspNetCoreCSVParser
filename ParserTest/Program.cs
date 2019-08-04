a4fca92b0c5d994c718e78df02047960fcd986fe
Batuhan Yahşi <yahsibatuhan@gmail.com>
2019-08-05 02:07:55 +0300

    Update Program.cs

--- ParserTest/Program.cs
+++ ParserTest/Program.cs
@@ -9,7 +9,7 @@ namespace ParserTest
         public static void Main(string[] args)
         {
             CsvContext csvContext = new CsvContext();
-            var test = csvContext.Read<TestModel>($"C:\\Users\\Batu\\documents\\visual studio 2017\\Projects\\AspNetCoreCSVParser\\ParserTest\\Files\\TestCsv.csv",new CsvFileDescription() { SeparatorChar = ','}).ToList();
+            var test = csvContext.Read<TestModel>($"C:\\TestCsv.csv",new CsvFileDescription() { SeparatorChar = ','}).ToList();
             test.ForEach(f => {
                 Console.WriteLine($"ID : {f.ID} - Name : {f.Name} - Surname : {f.Surname} - Gsm : {f.Gsm}");
             });
